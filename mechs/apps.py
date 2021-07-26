758051f5147008595060ba2532e0b65988aa4fcc
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-26 23:08:00 +0300

    initial commit

---
+++ mechs/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MechsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mechs'
---
+++ mechs/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render,redirect, get_object_or_404
+from .forms import SignupForm
+from django.contrib.auth import login, authenticate
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def index(request):
+    return render(request, 'mechs/index.html')
+    
