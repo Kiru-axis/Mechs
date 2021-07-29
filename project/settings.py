1dd0f00df05b0d2a654931ee843296bba38c919e
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-29 09:18:30 +0300

    mechanics models completed

--- project/settings.py
+++ project/settings.py
@@ -153,6 +153,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
