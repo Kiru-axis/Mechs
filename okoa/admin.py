0da504f7275cfc746cb996003c3af09bee90b11c
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-07-29 09:07:17 +0300

    comment registered to admin

--- okoa/admin.py
+++ okoa/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Mechanic,Rating,Location,Profile
+from .models import Mechanic,Rating,Location,Profile,Comment
 
 # Register your models here.
 admin.site.register(Mechanic)
 admin.site.register(Rating)
 admin.site.register(Location)
 admin.site.register(Profile)
+admin.site.register(Comment)
 
